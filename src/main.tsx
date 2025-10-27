54b23c7cc35434e76590d174e6f2ff843a59a87c
BekaChkhiro <bekachkhirodze1@gmail.com>
2025-10-27 11:07:19 +0400

    index html changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
-// Ensure React is globally available for Radix UI
-(window as any).React = React;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
